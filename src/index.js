c9333523d7e3958d187050618936fb064a288e1a
Kevin-Kinyanjui <kevin.kinyanjui@student.moringaschool.com>
2023-04-12 13:19:59 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+document.addEventListener('DOMContentLoaded', () => {
+  let form = document.querySelector("form")
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.newTaskDescription.value)
+    form.reset()
+  })
+})
+
+function buildToDo(todo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  
+  p.textContent = ` ${todo}   `     
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+  
+  }
+  
+  function handleDelete (e){
+  e.target.parentNode.remove()
+  }
+
+
+
+
